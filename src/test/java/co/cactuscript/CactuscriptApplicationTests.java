424010eb1fb9309d5348b4990bef2dd1d5372acd
William Levine <23029518+william-levine@users.noreply.github.com>
2024-11-16 17:40:09 +0000

    Added structure

---
+++ src/test/java/co/cactuscript/CactuscriptApplicationTests.java
@@ -0,0 +1,13 @@
+package co.cactuscript;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CactuscriptApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
